95cb8fecae9ef174730dc749d2c6d956760b9570
therealanvith <nanvith2007@gmail.com>
2026-06-08 02:52:05 +0530

    Some more fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
 import { handlers } from "@/auth"
-import { NextRequest } from "next/server"
 
-export async function GET(
-  request: NextRequest
-) {
-  return handlers.GET(request as unknown as Parameters<typeof handlers.GET>[0])
-}
-
-export async function POST(
-  request: NextRequest
-) {
-  return handlers.POST(request as unknown as Parameters<typeof handlers.POST>[0])
-}
+export const { GET, POST } = handlers
 
 export const dynamic = 'force-dynamic'
